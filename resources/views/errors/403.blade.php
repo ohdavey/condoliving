92a13f028c57a5fc25f1b0f1b56a6eeb749365ce
Davey Contreras <contreras.davey@gmail.com>
2018-02-18 10:23:52 -0500

    [SITE-05] - Add error handling user friendly display.

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+            <div class="container">
+                <div class="error-content">
+                    <h3><i class="fa fa-warning text-warning"></i> 403 - Sorry, you are not authorized here...</h3>
+                    <p>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="error-page">
+    </div>
+@endsection
