fcd9f4a2cd8381c69251e18278646e5091dce04e
Davey Contreras <contreras.davey@gmail.com>
2018-02-25 13:38:48 -0500

    [CRUMBS-01] - Create breadcrumbs for community and property.

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -5,13 +5,19 @@ Breadcrumbs::register('home', function ($breadcrumbs) {
     $breadcrumbs->push('Home', route('home'));
 });
 
-// Home > [Community]
+// Home > Communities
 Breadcrumbs::register('communities', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
     $breadcrumbs->push('Communities', route('communities'));
 });
 
+// Home > Community > [Community]
 Breadcrumbs::register('community.show', function ($breadcrumbs, $community) { // <-- The same Post model is injected here
     $breadcrumbs->parent('communities');
-    $breadcrumbs->push($community->name, route('community', $community));
+    $breadcrumbs->push($community->name, route('community.show', $community));
+});
+
+Breadcrumbs::register('property.show', function ($breadcrumbs, $community, $property) {
+    $breadcrumbs->parent('community.show', $community);
+    $breadcrumbs->push($property->address);
 });
