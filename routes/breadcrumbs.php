7a608b36e5c42aca11729f12a1a4b786f1c5b408
Davey Contreras <contreras.davey@gmail.com>
2018-03-10 12:12:14 -0500

    [LEASE-06] - Create breadcrumps route for properties and lease.

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,5 +1,6 @@
 <?php
 use App\Community;
+use App\Property;
 // Home
 Breadcrumbs::register('home', function ($breadcrumbs) {
     $breadcrumbs->push('Home', route('home'));
@@ -19,5 +20,10 @@ Breadcrumbs::register('community.show', function ($breadcrumbs, $community) { //
 
 Breadcrumbs::register('property.show', function ($breadcrumbs, $community, $property) {
     $breadcrumbs->parent('community.show', $community);
-    $breadcrumbs->push($property->address);
+    $breadcrumbs->push($property->address, route('property.show', ['community' => $community, 'property' => $property]));
+});
+
+Breadcrumbs::register('lease.create', function ($breadcrumbs, Community $community, Property $property) {
+    $breadcrumbs->parent('property.show', $community, $property);
+    $breadcrumbs->push('Create Lease');
 });
