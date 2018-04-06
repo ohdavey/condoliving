91023f9b4c4b8b6026a8bdb0c96633a24384db28
Davey Contreras <contreras.davey@gmail.com>
2018-04-06 05:52:41 -0400

    [LEASE-06] - Move validation into model, accept personal id instead of ssn - replace field to personal id.

--- app/Http/Controllers/LeaseController.php
+++ app/Http/Controllers/LeaseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Lease;
 use App\Property;
+use App\Tenant;
 use App\Community;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,7 @@ class LeaseController extends Controller
      */
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
@@ -57,30 +58,13 @@ class LeaseController extends Controller
      */
     public function store(Request $request)
     {
-        request()->validate([
-            'property_id' => 'required|numeric',
-            'deposit' => 'required|regex:/^\d*(\.\d{1,2})?$/',
-            'monthly_rate' => 'required|regex:/^\d*(\.\d{1,2})?$/',
-            'late_fee' => 'required|regex:/^\d*(\.\d{1,2})?$/',
-            'maintenance_fee' => 'required|regex:/^\d*(\.\d{1,2})?$/',
-            'amenities' => 'required|regex:/([a-zA-Z0-9]+,)?[a-zA-Z0-9]+/',
-            'start_date' => 'required|date',
-            'end_date' => 'required|date|after:start_date',
-            'due_day' => 'required|numeric',
-            'notes' => 'required|string',
-            'ssn' => 'required_if:tenant_id,0|unique:tenants',
-            'first_name' => 'required_if:tenant_id,0|string',
-            'last_name' => 'required_if:tenant_id,0|string',
-            'email' => 'required_if:tenant_id,0|email',
-            'phone' => 'required_if:tenant_id,0',
-            'dob' => 'required_if:tenant_id,0|date|before:-18year',
-            'salary' => 'required_if:tenant_id,0|regex:/([a-zA-Z0-9]+,)?[a-zA-Z0-9]+/',
-        ]);
+        $lease = new Lease;
+        request()->validate($lease->rules());
 
         if (!$request->tenant_id) {
             $tenant = new Tenant;
             $tenant->property_id = $request->property_id;
-            $tenant->ssn = $request->ssn;
+            $tenant->personal_id = $request->personal_id;
             $tenant->first_name = $request->first_name;
             $tenant->last_name = $request->last_name;
             $tenant->email = $request->email;
@@ -93,7 +77,7 @@ class LeaseController extends Controller
             $tenant = Tenant::where(['id' => $request->tenant_id, 'status' => '0'])->firstOrFail();
         }
         if ($tenant->save()) {
-            $lease = Lease::create([
+            $lease->create([
                 'creator_id' => auth()->id(),
                 'property_id' => request('property_id'),
                 'tenant_id' => $tenant->id,
@@ -108,6 +92,10 @@ class LeaseController extends Controller
                 'status' => 1,
                 'notes' => request('notes'),
             ]);
+
+            $property = Property::find($request->property_id);
+            $property->status = 1;
+            $property->save();
         }
 
         return view('lease.show', compact('lease'));
@@ -121,6 +109,7 @@ class LeaseController extends Controller
      */
     public function show(Lease $lease)
     {
+        $this->authorize('update', $lease);
         return view('lease.show', compact('lease'));
     }
 
