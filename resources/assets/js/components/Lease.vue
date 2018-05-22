37c85ca13f0711d2f51397b8186888e57c48a9dd
Davey Contreras <contreras.davey@gmail.com>
2018-05-21 23:44:38 -0400

    [LEASE-06] - Improvement to Lease object.

--- resources/assets/js/components/Lease.vue
+++ resources/assets/js/components/Lease.vue
@@ -1,18 +1,40 @@
 <script>
+    import Validate from 'vee-validate';
+    import moment from 'moment';
+    Vue.use(Validate);
     export default {
         props: ['tenants'],
         data() {
             return {
-                ssnumber: this.ssnumber,
+                pid: this.pid,
                 tenant: {
                     type: Object,
                     required: true
                 },
+                lease: {
+                    tenant_id: '',
+                    start_date: '',
+                    end_date: '',
+                    due_day: '',
+                    monthly_rate: '',
+                    deposit: '',
+                    maintenance_fee: '',
+                    late_fee: '',
+                    amenities: '',
+                    notes: '',
+                },
+                today: '',
                 existingTenant: false,
                 response: {
                     msg: "",
+                    errors: [],
                     status: "",
                     action: "",
+                },
+                request: {
+                    message: "",
+                    errors: [],
+                    status: ""
                 },
             }
         },
@@ -23,19 +45,46 @@
 
         },
         mounted() {
-            $("#tenant input").not("[name=ssnumber]").prop("disabled", true);
+            $("#tenant input").not("[name=pid]").prop("disabled", true);
+            this.today = moment().format('YYYY-MM-DD');
         },
 
         methods: {
-            lookUpTenant() {
-                if (!this.ssnumber) return;
+            validate: function () {
+                this.$validator.validateAll().then((result) => {
+                    if (result) {
+                        let formContents = jQuery.extend(this.lease, this.tenant);
+                        this.createLease(formContents);
+                        return;
+                    }
+                });
+            },
+            createLease: function(formContents) {
+                axios({
+                    method: 'post',
+                    responseType: 'json',
+                    url: '/lease',
+                    data: formContents
+                }).then(response => {
+                    self.$router.push('/lease' + response.data.lease);
+                }).catch(error => {
+                    this.request.message = error.response.data.message;
+                    this.request.errors = error.response.data.errors;
+                    this.request.status = 'error';
+                    console.log(this.request.message);
+                    console.log(this.request.errors);
+                });
+            },
+            lookUpTenant: function() {
+                if (!this.pid) return;
                 axios.post('/tenants/lookup', {
-                    ssnumber: this.ssnumber,
+                    pid: this.pid,
                 }).then(response => {
                     if (response.data.status == 'success') {
                         this.tenant = response.data.tenant;
+                        this.lease.tenant_id = this.tenant.id;
                         this.existingTenant = true;
-                        $("#tenant input").not("[name=ssnumber]").prop("disabled", false).prop("readonly", true);
+                        $("#tenant input").not("[name=pid]").prop("disabled", false).prop("readonly", true);
                     } else {
                         this.existingTenant = false;
                     }
@@ -44,7 +93,7 @@
                     flash(error.response.data, 'danger');
                 });
             },
-            prepResponse(msg, status) {
+            prepResponse: function(msg, status) {
                 console.log(msg, status);
                 if (status == 'success') {
                     this.response.msg = "A tenant was found, you may update the tenants information?";
@@ -57,8 +106,8 @@
                     this.response.status = "alert-warning";
                 }
             },
-            enableTenantInputs() {
-                $("#tenant input").not("[name=ssnumber]").prop("disabled", false).prop("readonly", false);
+            enableTenantInputs: function() {
+                $("#tenant input").not("[name=pid]").prop("disabled", false).prop("readonly", false);
             },
         }
     }
